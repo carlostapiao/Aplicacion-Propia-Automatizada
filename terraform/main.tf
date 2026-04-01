834c636c36defd65d68eab2cdcdbf10f005d6074
carlostapia <carlostapiaT3csup32@gmailcom>
2026-04-01 14:44:39 -0500

    Arquitectura v3: Despliegue final con Backend Remoto2

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,7 +8,7 @@ terraform {
   backend "azurerm" {
     resource_group_name  = "rg-apppersonal-tfstate"
     storage_account_name = "stcarlosv3state"
-    container_name       = "tfstate-appersonal"
+    container_name       = "tfstate-apppersonal"
     key                  = "terraform.tfstate"
   }
 
